4ebf1225eb6bbea42caf819f305e3b5d14d1fbe4
GT1876 <garrysontime@gmail.com>
2014-05-06 10:07:28 -0400

    Brandon Sucks At Basketball

---
+++ ChapterSixProblems/Numbers.cs/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Numbers.cs
+{
+    class Program
+    {
+        public static void sum(int a, int b)
+        {
+            Console.WriteLine(a + b); 
+        }
+        public static void difference(int a, int b)
+        {
+            Console.WriteLine(a - b); 
+        }
+
+        static void Main()
+        {
+            int q = 30, t = 39;
+            sum(q, t);
+            difference(q, t);
+        }
+    }
+}
+
+
+    
+  
---
+++ ChapterSixProblems/Numbers2.Cs/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Numbers2.Cs
+{
+    class Program
+    {
+        public static int product(int a, int b)
+        {
+            return a * b;
+        }
+        static void Main()
+        {
+            int q = 30, t = 39;
+            int result = product(q, t);
+            Console.WriteLine(result);
+        }
+    }
+}
